d2eb89ff7c1e27858d24d6da3c208d3997c633ed
Meherzan Gai <meherzan.gai@gmail.com>
2024-07-06 14:32:13 -0400

    Worked on the js file by adding classes for various objects in the program (the gaps, the bird, and the scenery)

--- flappyBird.js
+++ flappyBird.js
@@ -3,17 +3,66 @@ const context = document.querySelector("canvas").getContext("2d");
 context.canvas.height = 400;
 context.canvas.width = 1220;
 
-const bird = {
-    img: "assets/bird.jpeg",
-    width: 40,
-    height: 40,
-    hopping: false
+class Bird {
+    constructor(img, width, height){
+        this.img = img;
+        this.x = 100;
+        this.y = 100;
+        this.width = width;
+        this.height = height;
+    }
 };
 
-class gap {
-    constructor(x, y, width){
+Bird.prototype.draw = function() {
+    context.drawImage(img, this.x, this.y);
+}
+
+class Gap {
+    constructor(x, height, width){
+        this.x = x;
+        this.height = height;
+        this.width = width || 20;
+    }
+
+
+}
+
+
+
+Gap.prototype.draw = function(){
+    context.fillRect(this.x - this.width/2, 0, this.width, this.y - this.height/2); // TOP COLLISION PIPE
+
+    context.fillRect(this.x - this.width/2, this.y + this.height/2, this.width, 360 - (this.y + this.height/2)); // BOTTOM COLLISION PIPE)
+}
+
+class Scenery{
+    constructor(x, y, width, height, xMoveAmount, yMoveAmount){
         this.x = x;
         this.y = y;
         this.width = width;
+        this.height = height;
+        this.xMoveAmount = xMoveAmount;
+        this.yMoveAmount = yMoveAmount;
     }
 }
+
+Scenery.prototype.moveScenery = function(){
+    this.x -= xMoveAmount;
+    this.y -= yMoveAmount;
+}
+
+class Grass extends Scenery{
+    constructor(x, y, width, height, xMoveAmount, yMoveAmount, image){
+        super(x, y, width, height, xMoveAmount, yMoveAmount);
+        this.img = 'assets/grass.jpeg';
+    }
+}
+
+
+class Clouds extends Scenery{
+    constructor(x, y, width, height, xMoveAmount, yMoveAmount, image){
+        super(x, y, width, height, xMoveAmount, yMoveAmount);
+        this.img = 'assets/grass.jpeg';
+    }
+}
+
